7810cecd1f85a127eda43afd4b4fe8f41f0c3dcd
JaeYoung Hwang <forest62590@gmail.com>
2016-11-29 14:06:25 +0900

    Prototype

--- Mi Band 2 AE/src/main/java/com/example/android/bluetoothlegatt/domain/URLInfomation.java
+++ Mi Band 2 AE/src/main/java/com/example/android/bluetoothlegatt/domain/URLInfomation.java
@@ -4,8 +4,10 @@ package com.example.android.bluetoothlegatt.domain;
  * Created by Blossom on 2016-11-27.
  */
 
+
+
 public interface URLInfomation {
-    String serverURL = "http://203.253.128.151:7579/mobius-yt"; //http://192.168.125.142:44674";
+    String serverURL = "http://10.71.1.179:7579/mobius-yt"; //http://192.168.125.142:44674";
     String AEName = "MiBand2";
     String containerName = "WalkingSteps";
 }
